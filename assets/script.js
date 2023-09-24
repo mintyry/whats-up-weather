0a45dc55275d0e0e5ec28106105759f95f7bd9ea
mintyry <gitmintyry@gmail.com>
2023-09-23 20:52:19 -0700

    added js to grab and show city, temp, humidity, and wind speed for current temp

--- assets/script.js
+++ assets/script.js
@@ -14,17 +14,63 @@ THEN I am again presented with current and future conditions for that city */
 
 
 
-function getApi() {
+function getApi(city) {
     let apiKey = "7d62d24437d4b74a7a9fb254a29a521e";
-    let city;
     let requestUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
 
     fetch(requestUrl)
         .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                console.log('error');
+                alert('Please enter an actual city.')
+                return;
+            }
             return response.json();
+
         })
         .then(function (data) {
             console.log(data);
+            let lat = data.coord.lat;
+            let long = data.coord.lon;
+            getForecast(lat, long);
+
+            let city = document.querySelector("#city");
+            let temp = document.querySelector('#temp');
+            let humid = document.querySelector('#humid');
+            let wind = document.querySelector('#wind');
+            let day = document.querySelector('#date');
+
+            let currentDay = dayjs();
+            let now = currentDay.format('M/D/YY');
+
+            city.textContent = data.name;
+            day.textContent = now;
+            temp.textContent = Math.round(data.main.temp) + '°';
+            humid.textContent = `Humidity: ${data.main.humidity}`;
+            wind.textContent = `Wind Speed: ${Math.round(data.wind.speed)} mph`;
+
+        })
+};
+
+function getForecast(lat, lon) {
+    let apiKey = "7d62d24437d4b74a7a9fb254a29a521e";
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                console.log('error');
+                alert('Please enter an actual city.')
+                return;
+            }
+            return response.json();
+
+        })
+        .then(function (data) {
+            console.log(data);
+
         })
 };
 
@@ -34,8 +80,21 @@ searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     let input = document.querySelector('input').value;
     // console.log(input);
-    getApi();
+    getApi(input);
 });
 
 
+
+
+/*   let currentWeather =(`<p id="city">${data.name}</p>
+                <p id="date">x/xx/xx</p>
+                <p id="temp">${data.main.temp}°</p>
+                <div id="details-container">
+                    <p id="humid">${data.main.humidity}</p>
+                    <p id="wind">${data.wind.speed}</p>
+                </div>`);
+            let mainWeather = document.querySelector('#main-weather');
+            mainWeather.innerHTML= currentWeather; */
+
+// for loop item = item + 7
 
